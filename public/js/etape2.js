5ad128805f91f82e3597b298c577890403af53ad
Pier-Luc Brault <pier-luc@brault.me>
2025-05-20 18:18:55 -0400

    Retirer la solution

--- public/js/etape2.js
+++ public/js/etape2.js
@@ -13,76 +13,7 @@ async function testAPI() {
         console.error("Error in getAll: ", error);
     }
 
-    // Test get
-    console.log("Test get");
-    try {
-        const album = await albumsModel.get(1);
-        console.log("Album: ", album);
-    } catch (error) {
-        console.error("Error in get: ", error);
-    }
-
-    // Test create
-    /*console.log("Test create");
-    try {
-        const newAlbum = {
-            'name': 'Plages de sable 2',
-            'featured_photo_id': 3
-        };
-        const newAlbumId = await albumsModel.create(newAlbum);
-        console.log("New album created with ID: ", newAlbumId);
-    } catch (error) {
-        console.error("Error in create: ", error);
-    }*/
-
-    // Test update
-    /*console.log("Test update");
-    try {
-        const updatedAlbum = {
-            'name': 'Nouveau titre 2',
-            'featured_photo_id': 4
-        };
-        await albumsModel.update(1, updatedAlbum);
-        console.log("Album updated successfully");
-    } catch (error) {
-        console.error("Error in update: ", error);
-    }*/
-
-    // Test delete
-    /*console.log("Test delete");
-    try {
-        await albumsModel.delete(3);
-        console.log("Album deleted successfully");
-    } catch (error) {
-        console.error("Error in delete: ", error);
-    }*/
-
-    // Test getPhotos
-    console.log("Test getPhotos");
-    try {
-        const photos = await albumsModel.getPhotos(1);
-        console.log("Photos: ", photos);
-    } catch (error) {
-        console.error("Error in getPhotos: ", error);
-    }
-
-    // Test addPhotos
-    /*console.log("Test addPhotos");
-    try {
-        const photos = await albumsModel.addPhotos(2, [10]);
-        console.log("Photos added successfully: ");
-    } catch (error) {
-        console.error("Error in addPhotos: ", error);
-    }*/
-
-    // Test removePhoto
-    /*console.log("Test removePhoto");
-    try {
-        await albumsModel.removePhoto(1, 10);
-        console.log("Photo removed successfully");
-    } catch (error) {
-        console.error("Error in removePhoto: ", error);
-    }*/
 }
 
-testAPI();
+// Lorsque vous êtes prêt(e) à faire l'étape 2, décommentez la ligne ci-dessous et commentez la même ligne dans l'étape 1.
+// testAPI();
